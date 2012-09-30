c57525b181ae5c9b1072f9d88fdab018d80dfba0
Taras Mankovski <tarasm@gmail.com>
2012-09-30 11:57:27 -0400

    Added public sidebars Fixed header

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+from fabric.context_managers import cd
+from fabric.contrib.files import sed, exists
+
+env.hosts = ['wrktg.com']
+env.public_html = '~/public_html/'
+env.wp_config = '~/wp-config.php'
+env.wp_content = env.public_html + '/wp-content'
+env.user = 'wrktg'
+
+def fix_permissions(path=env.public_html):
+	'''Fix permissions in a directory'''
+	run('find %s -type d -exec chmod 755 {} \;' % path)
+	run('find %s -type f -exec chmod 644 {} \;' % path)
+
+def pull():
+    '''Pull latest git version to live site'''
+    with cd(env.wp_content):
+        run('git pull origin master')
+
+def push():
+    '''Push from local environment to Github'''
+    local('git push origin master')
--- themes/wp_pinfinity5-v1.0/header.php
+++ themes/wp_pinfinity5-v1.0/header.php
@@ -1,96 +1,108 @@
 <!doctype html>
-<!--[if lt IE 7]> <html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7" > <![endif]-->
-<!--[if IE 7]>    <html <?php language_attributes(); ?> class="no-js ie7 lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>    <html <?php language_attributes(); ?> class="no-js ie8 lt-ie9"> <![endif]-->
+<!--[if lt IE 7]>
+<html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7" > <![endif]-->
+<!--[if IE 7]>
+<html <?php language_attributes(); ?> class="no-js ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html <?php language_attributes(); ?> class="no-js ie8 lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <title>
-    <?php
-    ci_e_title();
+    <meta charset="utf-8">
+    <title>
+        <?php
+        ci_e_title();
 
-    // If using the YOAST WordPress SEO plugin, comment the line above, and uncomment the line below.
-    //wp_title('');
+        // If using the YOAST WordPress SEO plugin, comment the line above, and uncomment the line below.
+        //wp_title('');
 
-    // Likewise, most SEO plugins will ask you to edit the above wp_title() in some way that they need it to be.
-    // Do exactly as they require, as doing otherwise might have an impact on your search engine rankings.
-    ?>
-  </title>
+        // Likewise, most SEO plugins will ask you to edit the above wp_title() in some way that they need it to be.
+        // Do exactly as they require, as doing otherwise might have an impact on your search engine rankings.
+        ?>
+    </title>
 
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-  <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic|Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic|Yanone+Kaffeesatz' rel='stylesheet'
+          type='text/css'>
 
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/colors/default.css" type="text/css" media="screen" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/js/fancybox/source/jquery.fancybox.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/colors/default.css" type="text/css"
+          media="screen"/>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/js/fancybox/source/jquery.fancybox.css">
 
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
-  <?php if(ci_setting('favicon')): ?>
-  <link rel="shortcut icon" type="image/x-icon" href="<?php ci_e_setting('favicon'); ?>" />
-  <?php endif; ?>
+    <?php if (ci_setting('favicon')): ?>
+    <link rel="shortcut icon" type="image/x-icon" href="<?php ci_e_setting('favicon'); ?>"/>
+    <?php endif; ?>
 
-  <?php wp_head(); ?>
+    <?php wp_head(); ?>
 
 </head>
 
 <?php
-$alt_layout = ci_setting('layout')=='alt' ? ' alt ' : '';
+$alt_layout = ci_setting('layout') == 'alt' ? ' alt ' : '';
 ?>
-<body <?php body_class( $alt_layout ); ?>>
+<body <?php body_class($alt_layout); ?>>
 <?php do_action('after_open_body_tag'); ?>
 
-<?php if(is_singular()): ?>
+<?php if (is_singular()): ?>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
+<script>(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-  <?php endif; ?>
+    <?php endif; ?>
 
 <header id="header">
-  <div class="pre-head show-on-mobile">
-    <div class="wrap group">
-      <div class="pre-head-wgt group">
-        <?php dynamic_sidebar('top_bar'); ?>
-      </div> <!-- .header-wgt -->
+    <div class="pre-head show-on-mobile">
+        <div class="wrap group">
+            <div class="pre-head-wgt group">
+                <?php dynamic_sidebar('public_top'); ?>
+            </div>
+            <!-- .header-wgt -->
+        </div>
     </div>
-  </div> <!-- .pre-head -->
+    <!-- .pre-head -->
 
-  <div id="site-head">
-    <div class="wrap group">
-      <hgroup class="logo <?php echo ( ci_setting('logo')!='' ? 'imglogo' : 'textual' ); ?>">
-        <?php ci_e_logo('<h1>', '</h1>'); ?>
-      </hgroup>
+    <div id="site-head">
+        <div class="wrap group">
+            <hgroup class="logo textual">
+                <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
+            </hgroup>
 
-      <div class="header-wgt group">
-        <?php dynamic_sidebar('top_bar'); ?>
-      </div> <!-- .header-wgt -->
-    </div> <!-- .wrap < #header -->
-  </div> <!-- #site-head -->
+            <div class="header-wgt group">
+                <?php dynamic_sidebar('public_top'); ?>
+            </div>
+            <!-- .header-wgt -->
+        </div>
+        <!-- .wrap < #header -->
+    </div>
+    <!-- #site-head -->
 
-  <nav>
-    <div class="wrap group">
-      <?php
-      if(has_nav_menu('ci_main_menu'))
-        wp_nav_menu( array(
-          'theme_location' 	=> 'ci_main_menu',
-          'fallback_cb' 		=> '',
-          'container' 		=> '',
-          'menu_id' 			=> 'navigation',
-          'menu_class' 		=> 'group'
-        ));
-      else
-        wp_page_menu(array('menu_class'=>'group'));
-      ?>
-    </div> <!-- .wrap < nav -->
-  </nav>
+    <nav>
+        <div class="wrap group">
+            <?php
+            if (has_nav_menu('ci_main_menu'))
+                wp_nav_menu(array(
+                    'theme_location' => 'ci_main_menu',
+                    'fallback_cb' => '',
+                    'container' => '',
+                    'menu_id' => 'navigation',
+                    'menu_class' => 'group'
+                ));
+            else
+                wp_page_menu(array('menu_class' => 'group'));
+            ?>
+        </div>
+        <!-- .wrap < nav -->
+    </nav>
 </header>
 
 
 <section id="main">
-  <div class="wrap group">
+    <div class="wrap group">
